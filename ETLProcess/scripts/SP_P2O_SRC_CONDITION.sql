4ff8a1756de48d5d2ab74a2ebf9c00ecf5f57f08
stephanieshong <61520759+stephanieshong@users.noreply.github.com>
2020-06-27 10:15:06 -0400

    logic to insert to staging tbl for drug_exposure

--- ETLProcess/scripts/SP_P2O_SRC_CONDITION.sql
+++ ETLProcess/scripts/SP_P2O_SRC_CONDITION.sql
@@ -12,45 +12,49 @@ Parameters: DATAPARTNERID IN NUMBER, MANIFESTID IN NUMBER, RECORDCOUNT OUT NUMBE
                 2. CDMH_STAGING.ST_OMOP53_PROCEDURE_OCCURRENCE
                 3. CDMH_STAGING.ST_OMOP53_OBSERVATION
                 4. CDMH_STAGING.ST_OMOP53_MEASUREMENT
+                5. CDMH_STAGING.ST_OMOP53_DRUG_EXPOSURE
      Source:
      Revisions:
-     Ver          Date        Author          Description
-     0.1       6/16/2020     SHONG            Initial Version
-	 0.2     6/26/2020   SHONG             Added logic to insert into CDMH_STAGING.ST_OMOP53_MEASUREMENT
+     Ver          Date   Author     Description
+     0.1     6/16/2020   SHONG      Initial Version
+     0.2     6/25/2020   SHONG      Added logic to insert into CDMH_STAGING.ST_OMOP53_OBSERVATION
+     0.3     6/26/2020   SHONG      Added logic to insert into CDMH_STAGING.ST_OMOP53_MEASUREMENT and CDMH_STAGING.ST_OMOP53_DRUG_EXPOSURE
+
 
 ********************************************************************************************************/
-
-CREATE PROCEDURE                CDMH_STAGING.SP_P2O_SRC_CONDITION
+CREATE PROCEDURE                CDMH_STAGING.SP_P2O_SRC_CONDITION 
 (
   DATAPARTNERID IN NUMBER,
   MANIFESTID IN NUMBER,
   RECORDCOUNT OUT NUMBER
-) AS
+) AS 
+
 condition_recordCount number;
 observation_recordCount number;
 procedure_recordCount number;
 measure_recordCount number;
+drug_recordCount number ;
 BEGIN
 
    INSERT INTO CDMH_STAGING.ST_OMOP53_CONDITION_OCCURRENCE (  --INSERT INTO CDMH_STAGING.ST_OMOP53_CONDITION_OCCURRENCE
    data_partner_id,
    manifest_id,
-   condition_occurrence_id,
+   condition_occurrence_id, 
    person_id,
    condition_concept_id,
    condition_start_date, condition_start_datetime, condition_end_date, condition_end_datetime,
    condition_type_concept_id,
-   stop_reason, provider_id,
+   stop_reason, provider_id, 
     visit_occurrence_id, visit_detail_id,
    condition_source_value, condition_source_concept_id, condition_status_source_value, condition_status_concept_id,
-   DOMAIN_SOURCE)
-    SELECT
+   DOMAIN_SOURCE) 
+    SELECT 
     DATAPARTNERID as data_partner_id,
-    MANIFESTID as manifest_id,
+    MANIFESTID as manifest_id, 
     Mp.N3cds_Domain_Map_Id AS condition_occurrence_id,
-    p.N3cds_Domain_Map_Id AS person_id,
-    mp.target_concept_id as condition_concept_id,
-    c.report_date as condition_start_date, null as condition_start_datetime,
+    p.N3cds_Domain_Map_Id AS person_id,   
+    mp.target_concept_id as condition_concept_id, 
+    c.report_date as condition_start_date, null as condition_start_datetime, 
     c.resolve_date as condition_end_date, null as condition_end_datetime,
     case when enc.enc_type in ('ED', 'AV', 'IP', 'EI') then 38000251  -- need to check this with Charles / missing info
         when enc.enc_type in ('OT', 'OS', 'OA') then 38000269
@@ -58,22 +62,22 @@ BEGIN
     --NULL as condition_type_concept_id, ---ehr/ip/op/AG? setting of care origin ehr system /insclaim / registry / other sources/check the visit/ encounter type for this
     NULL as stop_reason, ---- encounter discharge type e.discharge type
     null as provider_id, ---is provider linked to patient
-    e.N3cds_Domain_Map_Id as visit_occurrence_id,
-    null as visit_detail_id,
+    e.N3cds_Domain_Map_Id as visit_occurrence_id, 
+    null as visit_detail_id, 
     c.CONDITION AS condition_source_value,
     xw.source_code_concept_id as condition_source_concept_id,
-   c.CONDITION_STATUS AS condition_status_source_value,
+   c.CONDITION_STATUS AS condition_status_source_value,    
     cst.TARGET_CONCEPT_ID AS CONDITION_STATUS_CONCEPT_ID,
     'PCORNET_CONDITION' as DOMAIN_SOURCE
 FROM NATIVE_PCORNET51_CDM.CONDITION c
-JOIN CDMH_STAGING.N3cds_Domain_Map mp on Mp.Source_Id= c.CONDITIONID
+JOIN CDMH_STAGING.N3cds_Domain_Map mp on Mp.Source_Id= c.CONDITIONID 
                 AND Mp.Domain_Name='CONDITION' AND mp.Target_Domain_Id = 'Condition' AND mp.DATA_PARTNER_ID=DATAPARTNERID
 LEFT JOIN CDMH_STAGING.N3cds_Domain_Map p on p.Source_Id=c.PATID AND p.Domain_Name='PERSON' AND p.DATA_PARTNER_ID=DATAPARTNERID
-LEFT JOIN CDMH_STAGING.N3cds_Domain_Map e on e.Source_Id=c.ENCOUNTERID AND e.Domain_Name='ENCOUNTER' and e.target_domain_id ='Visit' AND e.DATA_PARTNER_ID=DATAPARTNERID
+LEFT JOIN CDMH_STAGING.N3cds_Domain_Map e on e.Source_Id=c.ENCOUNTERID AND e.Domain_Name='ENCOUNTER' and e.target_domain_id ='Visit' AND e.DATA_PARTNER_ID=DATAPARTNERID 
 LEFT JOIN cdmh_staging.p2o_term_xwalk csrc on csrc.CDM_TBL='CONDITION' AND csrc.cdm_tbl_column_name='CONDITION_SOURCE' AND csrc.src_code=c.CONDITION_SOURCE
 LEFT JOIN cdmh_staging.p2o_term_xwalk cst on cst.CDM_TBL='CONDITION' AND cst.cdm_tbl_column_name='CONDITION_STATUS' AND cst.src_code=c.CONDITION_STATUS
-left join NATIVE_PCORNET51_CDM.encounter enc on enc.encounterid = e.source_id and e.domain_name='ENCOUNTER'
-left join cdmh_staging.visit_xwalk enctype on enc.enc_type = enctype.src_visit_type and enctype.CDM_NAME = 'PCORnet' and enctype.CDM_TBL = 'ENCOUNTER'
+left join NATIVE_PCORNET51_CDM.encounter enc on enc.encounterid = e.source_id and e.domain_name='ENCOUNTER' 
+left join cdmh_staging.visit_xwalk enctype on enc.enc_type = enctype.src_visit_type and enctype.CDM_NAME = 'PCORnet' and enctype.CDM_TBL = 'ENCOUNTER' 
 LEFT JOIN CDMH_STAGING.p2o_code_xwalk_standard xw on c.condition = xw.src_code and xw.CDM_TBL='CONDITION' AND xw.target_domain_id = 'Condition'
                                                                               and xw.target_concept_id=mp.target_concept_id
                                                                               and Xw.Src_Code_Type=c.condition_type
@@ -141,7 +145,7 @@ insert into CDMH_STAGING.ST_OMOP53_OBSERVATION (
     JOIN cdmh_staging.N3cds_Domain_Map mp on mp.Source_Id= c.CONDITIONID AND mp.Domain_Name='CONDITION' AND mp.Target_Domain_Id = 'Observation' AND mp.DATA_PARTNER_ID=DATAPARTNERID
     LEFT JOIN cdmh_staging.N3cds_Domain_Map p on p.Source_Id=c.PATID AND p.Domain_Name='PERSON' AND p.DATA_PARTNER_ID=DATAPARTNERID
     LEFT JOIN cdmh_staging.N3cds_Domain_Map e on e.Source_Id=c.ENCOUNTERID AND e.Domain_Name='ENCOUNTER' and e.target_domain_id ='Visit' AND e.DATA_PARTNER_ID=DATAPARTNERID
-    LEFT JOIN CDMH_STAGING.p2o_code_xwalk_standard xw on c.condition = xw.src_code and xw.CDM_TBL='CONDITION' AND xw.target_domain_id = 'Observation'
+    LEFT JOIN CDMH_STAGING.p2o_code_xwalk_standard xw on c.condition = xw.src_code and xw.CDM_TBL='CONDITION' AND xw.target_domain_id = 'Observation'  
                                         and mp.target_concept_id=xw.target_concept_id
                                         and Xw.Src_Code_Type=c.condition_type
  ;
@@ -168,11 +172,11 @@ COMMIT;
     MODIFIER_SOURCE_VALUE,
     DOMAIN_SOURCE
     )
-    SELECT
+    SELECT 
     DATAPARTNERID as data_partner_id,
     MANIFESTID as manifest_id,
     mp.N3cds_Domain_Map_Id AS PROCEDURE_OCCURRENCE_ID,
-    p.N3cds_Domain_Map_Id AS person_id,
+    p.N3cds_Domain_Map_Id AS person_id,  
     mp.target_concept_id as PROCEDURE_CONCEPT_ID,
     c.report_date as PROCEDURE_DATE,
     c.report_date as PROCEDURE_DATETIME,
@@ -203,7 +207,7 @@ COMMIT;
                                                        ;
     procedure_recordCount:=sql%rowcount;
     COMMIT;
-
+    
     INSERT INTO CDMH_STAGING.ST_OMOP53_MEASUREMENT (
         DATA_PARTNER_ID
         ,MANIFEST_ID
@@ -228,7 +232,7 @@ COMMIT;
         ,UNIT_SOURCE_VALUE
         ,VALUE_SOURCE_VALUE
         ,DOMAIN_SOURCE )
-        SELECT
+        SELECT 
             DATAPARTNERID as data_partner_id,
             MANIFESTID as manifest_id,
             mp.N3cds_Domain_Map_Id AS measurement_id,
@@ -265,15 +269,91 @@ COMMIT;
         LEFT JOIN CDMH_STAGING.N3cds_Domain_Map e on e.Source_Id=c.ENCOUNTERID AND e.Domain_Name='ENCOUNTER' and e.target_domain_id ='Visit' AND e.DATA_PARTNER_ID=DATAPARTNERID
 --        LEFT JOIN CDMH_STAGING.N3cds_Domain_Map prv on prv.Source_Id=e.source_id.providerid AND prv.Domain_Name='PROVIDER' AND prv.DATA_PARTNER_ID=2000
         LEFT JOIN CDMH_STAGING.P2O_CODE_XWALK_STANDARD xw on c.condition = xw.src_code and xw.CDM_TBL='CONDITION' AND xw.target_domain_id = 'Measurement'
-                                                            and xw.target_concept_id=mp.target_concept_id
+                                                            and xw.target_concept_id=mp.target_concept_id 
                                                             and Xw.Src_Code_Type=c.condition_type
 
 ;
 measure_recordCount:=SQL%ROWCOUNT;
 COMMIT;
 
-RECORDCOUNT  := observation_recordCount + condition_recordCount+procedure_recordCount+measure_recordCount;
-DBMS_OUTPUT.put_line(RECORDCOUNT ||' PCORnet CONDITION source data inserted to condition staging table, ST_OMOP53_CONDITION_OCCURRENCE and observation staging table, ST_OMOP53_OBSERVATION and procedure staging table, ST_OMOP53_PROCEDURE_OCCURRENCE, successfully.');
+--Condition to Drug target - map drug_type_concept_id  use CONDITION_SOURCE.
+
+INSERT INTO CDMH_STAGING.ST_OMOP53_DRUG_EXPOSURE ( -- INSERT INTO CDMH_STAGING.ST_OMOP53_DRUG_EXPOSURE 
+    data_partner_id,
+    manifest_id,
+    drug_exposure_id,
+    person_id,
+    drug_concept_id,
+    drug_exposure_start_date,
+	drug_exposure_start_datetime,
+    drug_exposure_end_date,
+	drug_exposure_end_datetime,
+    verbatim_end_date,
+	drug_type_concept_id,
+    stop_reason,refills,
+	quantity,
+	days_supply,
+	sig,
+    route_concept_id,
+    lot_number,
+    provider_id,
+    visit_occurrence_id,
+    visit_detail_id,
+    drug_source_value,
+    drug_source_concept_id,
+    route_source_value,
+    dose_unit_source_value,
+    DOMAIN_SOURCE
+	)
+
+SELECT
+    DATAPARTNERID as data_partner_id,
+    MANIFESTID as manifest_id,
+    mp.N3cds_Domain_Map_Id AS drug_exposure_id,
+    p.N3cds_Domain_Map_Id AS person_id,
+    mp.target_concept_id as drug_concept_id,
+    c.REPORT_DATE as drug_exposure_start_date,
+    c.REPORT_DATE as drug_exposure_start_datetime,
+    c.RESOLVE_DATE as drug_exposure_end_date,
+    null as drug_exposure_end_datetime,
+    null as verbatim_end_date,
+    45769798 as drug_type_concept_id, --ssh 6/25/2020 --issue number 54
+    null as stop_reason,
+    null as refills,
+    null as quantity,
+    null as days_supply,
+    null as sig,
+    null as route_concept_id,
+    null as lot_number,
+    null as provider_id, --m.medadmin_providerid as provider_id,
+    e.n3cds_domain_map_id as visit_occurrence_id,
+    null as visit_detail_id,
+    c.CONDITION as drug_source_value,
+    xw.source_code_concept_id as drug_source_concept_id,
+    null as route_source_value,
+    null as dose_unit_source_value,
+    'PCORNET_CONDTION' as DOMAIN_SOURCE
+  FROM NATIVE_PCORNET51_CDM.CONDITION c
+  JOIN CDMH_STAGING.N3cds_Domain_Map mp on mp.Source_Id= c.CONDITIONID AND mp.Domain_Name='CONDITION' AND mp.Target_Domain_Id = 'Drug' AND mp.DATA_PARTNER_ID=DATAPARTNERID
+  LEFT JOIN CDMH_STAGING.N3cds_Domain_Map p on p.Source_Id=c.PATID AND p.Domain_Name='PERSON' AND p.DATA_PARTNER_ID=DATAPARTNERID
+  LEFT JOIN CDMH_STAGING.N3cds_Domain_Map e on e.Source_Id=c.ENCOUNTERID AND e.Domain_Name='ENCOUNTER' and e.target_domain_id ='Visit' AND e.DATA_PARTNER_ID=DATAPARTNERID
+  LEFT JOIN CDMH_STAGING.p2o_code_xwalk_standard xw on c.condition = xw.src_code and xw.CDM_TBL='CONDITION' 
+														AND xw.target_domain_id = 'Drug'
+														and xw.target_concept_id=mp.target_concept_id 
+														and xw.Src_Code_Type=c.condition_type
+
+  ;
+
+drug_recordCount :=SQL%ROWCOUNT;
+
+Commit ;
+
+RECORDCOUNT  := observation_recordCount + condition_recordCount+procedure_recordCount+measure_recordCount+drug_recordCount;
+DBMS_OUTPUT.put_line(RECORDCOUNT ||' PCORnet CONDITION source data inserted to condition staging table - ST_OMOP53_CONDITION_OCCURRENCE 
+                                                                               observation staging table - ST_OMOP53_OBSERVATION 
+                                                                               procedure staging table - ST_OMOP53_PROCEDURE_OCCURRENCE
+                                                                               drug staging table - ST_OMOP53_DRUG_EXPOSURE
+                                                                               , successfully.'); 
 --
 
 
