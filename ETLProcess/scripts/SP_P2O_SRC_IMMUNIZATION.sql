b2ee67714a7f97ae852236cc2625f0bd46f50215
stephanieshong <61520759+stephanieshong@users.noreply.github.com>
2020-09-22 14:23:47 -0400

    most recent update

--- ETLProcess/scripts/SP_P2O_SRC_IMMUNIZATION.sql
+++ ETLProcess/scripts/SP_P2O_SRC_IMMUNIZATION.sql
@@ -1,19 +1,6 @@
-/**********************************************************************************
-project : N3C DI&H
-Date: 6/1/2020
-Author: Stephanie Hong / Sandeep Naredla / Richard Zhu / Tanner Zhang
-Description : Stored Procedure to insert PCORnet Innunization into staging table
-Stored Procedure: SP_P2O_SRC_IMMUNIZATION:
-Parameters: DATAPARTNERID IN NUMBER, MANIFESTID IN NUMBER, RECORDCOUNT OUT NUMBER
-Edit History::
-6/1/2020   SHONG Initial Version
-
-
-     Revisions:
-     Ver        Date        Author  Description
-     0.1        6/1/2020   SHONG   Initial Version
-
-*********************************************************************************************************/
+--------------------------------------------------------
+--  File SP_P2O_SRC_IMMUNIZATION - Tuesday-September-22-2020   
+--------------------------------------------------------
 
 CREATE PROCEDURE CDMH_STAGING.SP_P2O_SRC_IMMUNIZATION 
 
