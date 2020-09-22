3aa0ccf589b498bbbf69739b9edeaa00ed516d75
stephanieshong <61520759+stephanieshong@users.noreply.github.com>
2020-09-22 14:58:47 -0400

    most recent update

--- ETLProcess/scripts/SP_P2O_SRC_PROVIDER.sql
+++ ETLProcess/scripts/SP_P2O_SRC_PROVIDER.sql
@@ -1,73 +1,72 @@
-/***********************************************************************************************************
-project : N3C DI&H
-Date: 5/16/2020
-Author: Stephanie Hong
-FILE:           SP_P2O_SRC_PROVIDER.sql
-Description :   Loading NATIVE_PCORNET51_CDM.PROVIDER table into stging table SP_P2O_SRC_PROVIDER
-Procedure:      SP_P2O_SRC_PROVIDER
-Edit History:
-     Ver       Date         Author          Description
-     0.1       5/16/2020    SHONG           Initial version
- 
-*************************************************************************************************************/
 
-CREATE PROCEDURE CDMH_STAGING.SP_P2O_SRC_PROVIDER 
-(
-  DATAPARTNERID IN NUMBER 
-, MANIFESTID IN NUMBER 
-, RECORDCOUNT OUT NUMBER
-) AS 
+CREATE PROCEDURE              CDMH_STAGING.SP_P2O_SRC_PROVIDER (
+    datapartnerid   IN    NUMBER,
+    manifestid      IN    NUMBER,
+    recordcount     OUT   NUMBER
+) AS
+/********************************************************************************************************
+     Name:      sp_p2o_src_provider
+     Purpose:    Loading The NATIVE_PCORNET51_CDM.PROVIDER Table into 
+                1. CDMH_STAGING.st_omop53_provider
+
+     Edit History:
+     Ver          Date        Author               Description
+       0.1         8/30/20     SHONG               Intial Version.
+       
+*********************************************************************************************************/
 BEGIN
-  
-  INSERT INTO CDMH_STAGING.ST_OMOP53_PROVIDER
-  ( DATA_PARTNER_ID,
-  MANIFEST_ID,
-  PROVIDER_ID,
-  PROVIDER_NAME,
-  NPI,
-  DEA,
-  SPECIALTY_CONCEPT_ID,
-  CARE_SITE_ID,
-  YEAR_OF_BIRTH,
-  GENDER_CONCEPT_ID,
-  PROVIDER_SOURCE_VALUE,
-  SPECIALTY_SOURCE_VALUE,
-  SPECIALTY_SOURCE_CONCEPT_ID,
-  GENDER_SOURCE_VALUE,
-  GENDER_SOURCE_CONCEPT_ID,
-  DOMAIN_SOURCE
-)
+    DELETE FROM cdmh_staging.st_omop53_provider
+    WHERE
+        data_partner_id = datapartnerid
+        AND domain_source = 'PCORNET_PROVIDER';
 
-  SELECT 
-  DATAPARTNERID AS DATA_PARTNER_ID,
-  MANIFESTID AS MANIFEST_ID,
-  mp.N3cds_Domain_Map_Id AS PROVIDER_ID, 
-  NULL AS PROVIDER_NAME,
-  NULL AS NPI,
-  NULL AS DEA,
-  NULL AS SPECIALTY_CONCEPT_ID,
-  NULL AS CARE_SITE_ID,
-  NULL AS YEAR_OF_BIRTH,
-  gx.TARGET_CONCEPT_ID AS gender_concept_id, 
-  NULL AS PROVIDER_SOURCE_VALUE,
-  PROVIDER_SPECIALTY_PRIMARY AS specialty_source_value ,
-  NULL AS SPECIALTY_SOURCE_CONCEPT_ID,
-  PROVIDER_SEX AS gender_source_value, 
-  NULL AS GENDER_SOURCE_CONCEPT_ID,
-  'PCORNET-Provider' AS DOMAIN_SOURCE
+    COMMIT;
+    INSERT INTO cdmh_staging.st_omop53_provider (
+        data_partner_id,
+        manifest_id,
+        provider_id,
+        provider_name,
+        npi,
+        dea,
+        specialty_concept_id,
+        care_site_id,
+        year_of_birth,
+        gender_concept_id,
+        provider_source_value,
+        specialty_source_value,
+        specialty_source_concept_id,
+        gender_source_value,
+        gender_source_concept_id,
+        domain_source
+    )
+        SELECT
+            datapartnerid                AS data_partner_id,
+            manifestid                   AS manifest_id,
+            mp.n3cds_domain_map_id       AS provider_id,
+            NULL AS provider_name,
+            NULL AS npi,
+            NULL AS dea,
+            NULL AS specialty_concept_id,
+            NULL AS care_site_id,
+            NULL AS year_of_birth,
+            gx.target_concept_id         AS gender_concept_id,
+            NULL AS provider_source_value,
+            provider_specialty_primary   AS specialty_source_value,
+            NULL AS specialty_source_concept_id,
+            provider_sex                 AS gender_source_value,
+            NULL AS gender_source_concept_id,
+            'PCORNET_PROVIDER' AS domain_source
+        FROM
+            "NATIVE_PCORNET51_CDM"."PROVIDER"
+            JOIN cdmh_staging.n3cds_domain_map   mp ON mp.source_id = provider.providerid
+                                                     AND mp.domain_name = 'PROVIDER'
+                                                     AND mp.data_partner_id = datapartnerid
+            LEFT JOIN cdmh_staging.gender_xwalk       gx ON gx.cdm_tbl = 'DEMOGRAPHIC'
+                                                      AND gx.src_gender = provider.provider_sex;
 
-  FROM "NATIVE_PCORNET51_CDM"."PROVIDER" 
-  JOIN CDMH_STAGING.N3cds_Domain_Map mp on Mp.Source_Id=PROVIDER.PROVIDERID 
-                                          AND Mp.Domain_Name='PROVIDER'
-                                          AND mp.DATA_PARTNER_ID=DATAPARTNERID
-  LEFT JOIN CDMH_STAGING.Gender_Xwalk gx on gx.CDM_TBL='DEMOGRAPHIC'
-                                          AND Gx.Src_Gender=PROVIDER.PROVIDER_SEX
-                                          ;
-  RECORDCOUNT  := sql%rowcount;
-    
-  DBMS_OUTPUT.put_line(RECORDCOUNT || ' PCORnet PROVIDER source data inserted to staging table, ST_OMOP53_PROVIDER, successfully.'); 
+    recordcount := SQL%rowcount;
+    dbms_output.put_line(recordcount || ' PCORnet PROVIDER source data inserted to staging table, ST_OMOP53_PROVIDER, successfully.'
+    ); 
 --
-
-
-  commit ;                                        
-END SP_P2O_SRC_PROVIDER;
+    COMMIT;
+END sp_p2o_src_provider;
