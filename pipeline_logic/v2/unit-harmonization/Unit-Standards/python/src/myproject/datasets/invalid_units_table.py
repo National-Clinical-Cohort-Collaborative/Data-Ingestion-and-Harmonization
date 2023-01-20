8131285672f57210e52da5e50d053066c7cd9716
Stephanie Hong <shong59@jh.edu>
2023-01-20 15:50:14 -0500

    update list name from canonical_units to invalid_units

--- pipeline_logic/v2/unit-harmonization/Unit-Standards/python/src/myproject/datasets/invalid_units_table.py
+++ pipeline_logic/v2/unit-harmonization/Unit-Standards/python/src/myproject/datasets/invalid_units_table.py
@@ -10,12 +10,16 @@ def my_compute_function(output):
     Below is a list of invalid units from sites per codeset.
     These units will be nulled out during the unit inference and harmonization process such that unit inference is performed.
     '''
-    canonical_units = [
+    invalid_units = [
         {'measured_variable':'White blood cell count, x10E3/uL','codeset_id':'138719030','omop_unit_concept_id':'8792','omop_unit_concept_name':'Kelvin per microliter'},
         {'measured_variable':'White blood cell count, x10E3/uL','codeset_id':'138719030','omop_unit_concept_id':'8903','omop_unit_concept_name':'Kelvin per cubic millimeter'},
+        {'measured_variable':'White blood cell count, x10E3/uL','codeset_id':'138719030','omop_unit_concept_id':'8938','omop_unit_concept_name':'thousandth per microliter'},
+        {'measured_variable':'White blood cell count, x10E3/uL','codeset_id':'138719030','omop_unit_concept_id':'8566','omop_unit_concept_name':'thousand'},
         {'measured_variable':'Neutrophils (absolute), x10E3/uL','codeset_id':'881397271','omop_unit_concept_id':'8792','omop_unit_concept_name':'Kelvin per microliter'},
         {'measured_variable':'Lymphocytes (absolute), x10E3/uL','codeset_id':'627523060','omop_unit_concept_id':'8792','omop_unit_concept_name':'Kelvin per microliter'},
+        {'measured_variable':'Lymphocytes (absolute), x10E3/uL','codeset_id':'627523060','omop_unit_concept_id':'8566','omop_unit_concept_name':'thousand'},
         {'measured_variable':'Platelet count, x10E3/uL','codeset_id':'167697906','omop_unit_concept_id':'8792','omop_unit_concept_name':'Kelvin per microliter'}
+        {'measured_variable':'Platelet count, x10E3/uL','codeset_id':'167697906','omop_unit_concept_id':'8938','omop_unit_concept_name':'thousandth per microliter'}
 
     ]
 
