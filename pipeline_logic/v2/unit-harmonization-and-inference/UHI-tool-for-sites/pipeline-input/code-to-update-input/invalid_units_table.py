ffc4de7ca60f558110896c2c7b038ef0378f8e9c
kbradwell <kbradwell@users.noreply.github.com>
2023-01-23 18:56:29 -0500

    Update invalid_units_table.py

--- pipeline_logic/v2/unit-harmonization-and-inference/UHI-tool-for-sites/pipeline-input/code-to-update-input/invalid_units_table.py
+++ pipeline_logic/v2/unit-harmonization-and-inference/UHI-tool-for-sites/pipeline-input/code-to-update-input/invalid_units_table.py
@@ -10,7 +10,7 @@ def my_compute_function(output):
     Below is a list of invalid units from sites per codeset.
     These units will be nulled out during the unit inference and harmonization process such that unit inference is performed.
     '''
-    canonical_units = [
+    invalid_units = [
         {'measured_variable':'White blood cell count, x10E3/uL','codeset_id':'138719030','omop_unit_concept_id':'8792','omop_unit_concept_name':'Kelvin per microliter'},
         {'measured_variable':'White blood cell count, x10E3/uL','codeset_id':'138719030','omop_unit_concept_id':'8903','omop_unit_concept_name':'Kelvin per cubic millimeter'},
         {'measured_variable':'Neutrophils (absolute), x10E3/uL','codeset_id':'881397271','omop_unit_concept_id':'8792','omop_unit_concept_name':'Kelvin per microliter'},
